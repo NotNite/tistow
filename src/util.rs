9d86b8583d0cbb5c21ad569c6c7d51d93f3b8216
Philpax <me@philpax.me>
2022-07-07 00:15:35 +0200

    refactor: use HashSet for is_hotkey_pressed

--- src/util.rs
+++ src/util.rs
@@ -1,21 +1,13 @@
-use std::path::PathBuf;
+use std::{collections::HashSet, path::PathBuf};
 
 use device_query::{DeviceQuery, DeviceState, Keycode};
 
 pub fn is_hotkey_pressed(device_state: &DeviceState) -> bool {
-    let keys: Vec<Keycode> = device_state.get_keys();
-    // TODO: turn into cli args
-    let hotkey = vec![Keycode::LControl, Keycode::LAlt, Keycode::Backspace];
-
-    let mut hotkey_pressed = true;
-
-    for key in hotkey {
-        if !keys.contains(&key) {
-            hotkey_pressed = false;
-        }
-    }
-
-    hotkey_pressed
+    HashSet::<Keycode>::from_iter(device_state.get_keys()).is_superset(&HashSet::from_iter([
+        Keycode::LControl,
+        Keycode::LAlt,
+        Keycode::Backspace,
+    ]))
 }
 
 #[cfg(target_os = "windows")]
